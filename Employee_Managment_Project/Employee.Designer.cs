0b3c69b51dd881ec7065c6cc6328c59c9c0ae41c
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-01-05 19:39:09 +0200

    create textbox and buttons

--- Employee_Managment_Project/Employee.Designer.cs
+++ Employee_Managment_Project/Employee.Designer.cs
@@ -28,12 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.Editbtn = new System.Windows.Forms.Button();
+            this.JData = new System.Windows.Forms.DateTimePicker();
+            this.label11 = new System.Windows.Forms.Label();
+            this.EmpSal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EmpNameTb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EmbGen = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.EmpDep = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.DOB = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(31, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1462, 136);
+            this.panel1.TabIndex = 81;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(590, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(472, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee Management System Version 1.0";
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.BackColor = System.Drawing.Color.Teal;
+            this.Addbtn.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Addbtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Addbtn.Location = new System.Drawing.Point(64, 662);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(140, 51);
+            this.Addbtn.TabIndex = 86;
+            this.Addbtn.Text = "Add";
+            this.Addbtn.UseVisualStyleBackColor = false;
+            // 
+            // Editbtn
+            // 
+            this.Editbtn.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.Editbtn.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Editbtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Editbtn.Location = new System.Drawing.Point(219, 662);
+            this.Editbtn.Name = "Editbtn";
+            this.Editbtn.Size = new System.Drawing.Size(140, 51);
+            this.Editbtn.TabIndex = 87;
+            this.Editbtn.Text = "Update";
+            this.Editbtn.UseVisualStyleBackColor = false;
+            // 
+            // JData
+            // 
+            this.JData.Location = new System.Drawing.Point(64, 551);
+            this.JData.Name = "JData";
+            this.JData.Size = new System.Drawing.Size(295, 27);
+            this.JData.TabIndex = 103;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label11.Location = new System.Drawing.Point(64, 601);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(214, 25);
+            this.label11.TabIndex = 104;
+            this.label11.Text = "Employee Daily sallary :";
+            // 
+            // EmpSal
+            // 
+            this.EmpSal.Location = new System.Drawing.Point(64, 629);
+            this.EmpSal.Name = "EmpSal";
+            this.EmpSal.Size = new System.Drawing.Size(295, 27);
+            this.EmpSal.TabIndex = 105;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(64, 719);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 51);
+            this.button1.TabIndex = 106;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(64, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 25);
+            this.label3.TabIndex = 83;
+            this.label3.Text = "Employee Name :";
+            // 
+            // EmpNameTb
+            // 
+            this.EmpNameTb.Location = new System.Drawing.Point(64, 243);
+            this.EmpNameTb.Name = "EmpNameTb";
+            this.EmpNameTb.Size = new System.Drawing.Size(295, 27);
+            this.EmpNameTb.TabIndex = 84;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(927, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 31);
+            this.label2.TabIndex = 82;
+            this.label2.Text = "Manage Employee";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label4.Location = new System.Drawing.Point(64, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 25);
+            this.label4.TabIndex = 96;
+            this.label4.Text = "Employee Gender :";
+            // 
+            // EmbGen
+            // 
+            this.EmbGen.FormattingEnabled = true;
+            this.EmbGen.Items.AddRange(new object[] {
+            "Male",
+            "FeMale"});
+            this.EmbGen.Location = new System.Drawing.Point(64, 321);
+            this.EmbGen.Name = "EmbGen";
+            this.EmbGen.Size = new System.Drawing.Size(295, 28);
+            this.EmbGen.TabIndex = 97;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label8.Location = new System.Drawing.Point(64, 370);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(213, 25);
+            this.label8.TabIndex = 98;
+            this.label8.Text = "Employee Department :";
+            // 
+            // EmpDep
+            // 
+            this.EmpDep.FormattingEnabled = true;
+            this.EmpDep.Location = new System.Drawing.Point(64, 398);
+            this.EmpDep.Name = "EmpDep";
+            this.EmpDep.Size = new System.Drawing.Size(295, 28);
+            this.EmpDep.TabIndex = 99;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label9.Location = new System.Drawing.Point(64, 447);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 25);
+            this.label9.TabIndex = 100;
+            this.label9.Text = "Date Of Birth :";
+            // 
+            // DOB
+            // 
+            this.DOB.Location = new System.Drawing.Point(64, 475);
+            this.DOB.Name = "DOB";
+            this.DOB.Size = new System.Drawing.Size(295, 27);
+            this.DOB.TabIndex = 101;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label10.Location = new System.Drawing.Point(64, 523);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(102, 25);
+            this.label10.TabIndex = 102;
+            this.label10.Text = "Join Date :";
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1525, 776);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EmpSal);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.JData);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.DOB);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.EmpDep);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.EmbGen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Editbtn);
+            this.Controls.Add(this.Addbtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.EmpNameTb);
+            this.Controls.Add(this.label3);
+            this.Name = "Employee";
             this.Text = "Employee";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Button Addbtn;
+        private Button Editbtn;
+        private DateTimePicker JData;
+        private Label label11;
+        private TextBox EmpSal;
+        private Button button1;
+        private Label label3;
+        private TextBox EmpNameTb;
+        private Label label2;
+        private Label label4;
+        private ComboBox EmbGen;
+        private Label label8;
+        private ComboBox EmpDep;
+        private Label label9;
+        private DateTimePicker DOB;
+        private Label label10;
     }
 }
